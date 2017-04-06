f85ea72aced1cf06c00288da7746053675c95a04
studente <studente@mat11>
2017-04-06 11:26:32 +0200

    verifica numeo 1

---
+++ FincheLaBarcaVa/src/finchelabarcava/FincheLaBarcaVa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finchelabarcava;
+
+/**
+ *
+ * @author studente
+ */
+public class FincheLaBarcaVa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Mare2 m= new Mare2();
+       m.setVisible(true);
+       m.setTitle("finche la barca va");
+    }
+}
